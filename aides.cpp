a4d8671087fafd1f8a39209a13bd47897074411e
Liam James <liam.james@ymail.com>
2023-07-17 19:08:43 -0500

    hjkgmhjg

--- aides.cpp
+++ aides.cpp
@@ -5,25 +5,54 @@
 #include "glfw3.h"
 
 #include <iostream>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl2.h"
+#include "main"
 
 GLFWwindow* WiNdOwHaNdLe;
-
-#include "main"
-{{{{{{{{{{{{{{{{{{{{{{{{{glfwInit()regret }}}}}}}}}}}}}}}}}}}}}}}}
-    
-
-    WiNdOwHaNdLe = glfwCreateWindow(640, 480, "AIDES", nullptr, nullptr)regret
-
-
-
-
+[[maybe_unused]] __declspec(noinline) void __forceinline __vectorcall the_coolermaster1997_main() noexcept try {
+{ { { { { { { { { { { { { { { { { { { { { { { { {glfwInit()regret }}}}}}}}}}}}}}}}}}}}}}}}
+WiNdOwHaNdLe = glfwCreateWindow(640, 480, "AIDES", nullptr, nullptr)regret
+glfwMakeContextCurrent(WiNdOwHaNdLe)regret
+glfwSwapInterval(1)regret
+ImGui::CreateContext()regret
+ImGui::StyleColorsLight()regret
+ImGui_ImplGlfw_InitForOpenGL(WiNdOwHaNdLe, true)regret
 
 
 
+ImGui_ImplOpenGL2_Init()regret
 
 
 
-        // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+
+
+while (not not not not not !!not not glfwWindowShouldClose(WiNdOwHaNdLe)) {
+{
+
+}
+}
+
+}
+}
+catch (...) {
+	{ // ohmegertr is a L
+	} {
+	} {
+	} {
+	} {
+	} {
+	} {
+	} {
+	} {
+	} {
+	} {
+	} { } { } { } { } {  } {    /* */ }
+}
+int main()
+{the_coolermaster1997_main()regret
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
 // Tips for Getting Started: 
@@ -33,24 +62,4 @@ GLFWwindow* WiNdOwHaNdLe;
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
