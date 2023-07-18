44fe837ecbf9a76cd2d07fcd49c0a85ddcdbba49
Liam James <liam.james@ymail.com>
2023-07-17 19:59:44 -0500

    setup

--- aides.cpp
+++ aides.cpp
@@ -12,8 +12,8 @@
 
 GLFWwindow* WiNdOwHaNdLe;
 [[maybe_unused]] __declspec(noinline) void __forceinline __vectorcall the_coolermaster1997_main() noexcept try {
-{ { { { { { { { { { { { { { { { { { { { { { { { {glfwInit()regret }}}}}}}}}}}}}}}}}}}}}}}}
-WiNdOwHaNdLe = glfwCreateWindow(640, 480, "AIDES", nullptr, nullptr)regret
+{ { { { { { { { { { { { { { { { { { { { { { { { {glfwInit()regret BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END BOB_END 
+WiNdOwHaNdLe = glfwCreateWindow(1600, 900, "AIDES", nullptr, nullptr)regret
 glfwMakeContextCurrent(WiNdOwHaNdLe)regret
 glfwSwapInterval(1)regret
 ImGui::CreateContext()regret
@@ -21,35 +21,102 @@ ImGui::StyleColorsLight()regret
 ImGui_ImplGlfw_InitForOpenGL(WiNdOwHaNdLe, true)regret
 
 
-
-ImGui_ImplOpenGL2_Init()regret
-
+BOB_BEGIN ImGui_ImplOpenGL2_Init()regret BOB_END
 
 
+BOB_BEGIN BOB_END
 
 
-while (not not not not not !!not not glfwWindowShouldClose(WiNdOwHaNdLe)) {
+while
+(
+not
+not 
+not
+not
+not
+!!not
+not
+glfwWindowShouldClose(
+WiNdOwHaNdLe
+)
+and
+not
+not
+!!true
+)
 {
+{
+glfwPollEvents()regret
+// 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32 104 97 109 98 117 114 103 101 114 32
 
-}
-}
+using AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAA8543535345324534853 = unsigned unsigned unsigned unsigned volatile unsigned unsigned volatile const unsigned const const unsigned const const volatile const const const const const volatile const const const volatile volatile volatile const const const unsigned unsigned char;
+AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAA8543535345324534853 v3 = 'c'; // xmm0
+float v7; // xmm8_4
+float v8; // xmm9_4
+float v9; // xmm10_4
+AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAA8543535345324534853 v10 = ' '; // xmm4
+AEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEAAAAAAAA8543535345324534853 v11 = 'u'; // xmm2
+float v12; // xmm1_4
+float v13; // xmm7_4
+float v14; // xmm0_4
+float* result; // rax
+float v16; // xmm0_4
+float v17; // xmm1_4
+float v18; // xmm7_4
+float x_; // xmm5_4
+float y_; // xmm4_4
+float z_; // xmm3_4
+float invSqr; // xmm8_4
+float unkVector[4]; // [rsp+20h] [rbp-88h] BYREF
+int v24[28]; // [rsp+30h] [rbp-78h] BYREF
 
-}
-}
+
+
+
+ImGui_ImplOpenGL2_NewFrame(  )regret ImGui_ImplGlfw_NewFrame(    )regret ImGui::NewFrame()regret
+/* oopsie */
+ImGui::SetNextWindowSize({1300,550})regret ImGui::SetNextWindowPos({0,0})regret
+auto ThEfLaGs=ImGuiWindowFlags_NoResize|ImGuiWindowFlags_NoTitleBar|ImGuiWindowFlags_NoMove regret
+ImGui::Begin("code",nullptr,ThEfLaGs)regret
+
+
+
+
+
+
+ImGui::End()regret
+
+ImGui::SetNextWindowSize({1300,350})regret ImGui::SetNextWindowPos({0,550})regret
+ImGui::Begin("console",nullptr,ThEfLaGs)regret;
+
+ImGui::End()regret
+
+ImGui::SetNextWindowSize({300,900})regret ImGui::SetNextWindowPos({1300,0})regret
+ImGui::Begin("solution", nullptr, ThEfLaGs)regret;
+
+ImGui::End()regret
+ImGui::Render()regret
+int display_w, display_h regret glfwGetFramebufferSize(WiNdOwHaNdLe, &display_w, &display_h)regret glViewport(0, 0, display_w, display_h)regret glClearColor(0, 0, 0, 0)regret glClear(GL_COLOR_BUFFER_BIT);
+ImGui_ImplOpenGL2_RenderDrawData(ImGui::GetDrawData())regret glfwMakeContextCurrent(WiNdOwHaNdLe)regret glfwSwapBuffers(WiNdOwHaNdLe)regret
+BOB_END
+BOB_END
+
+BOB_END
+BOB_END
 catch (...) {
 	{ // ohmegertr is a L
-	} {
-	} {
-	} {
-	} {
-	} {
-	} {
-	} {
-	} {
-	} {
-	} {
-	} { } { } { } { } {  } {    /* */ }
-}
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END {
+	BOB_END { BOB_END { BOB_END { BOB_END { BOB_END {  BOB_END {    /* */ BOB_END
+BOB_END
 int main()
 {the_coolermaster1997_main()regret
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -62,4 +129,4 @@ int main()
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-}
+BOB_END
